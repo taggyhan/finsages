f298eb732bf56724fea6a9412edf9fdba7ec5e6d
yuvrajaryan <142132073+yuvrajaryan@users.noreply.github.com>
2024-06-28 01:49:04 +0800

    updated chatbot and goals

--- finsage/settings.py
+++ finsage/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure--=uy!j07-y9n+18uo0i(f2k7w7^d35(7xxm*!qg3o&15f4v(r$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
--- proj/urls.py
+++ proj/urls.py
@@ -14,5 +14,10 @@ urlpatterns = [
     path('delete-transaction/<int:transaction_id>/', views.delete_transaction, name='delete_transaction'),
     path('analysis/', views.analysis, name='analysis'),
     path('chatbot/', views.chatbot_view, name='chatbot'),
+    path('goals/', views.goals_view, name='goals_view'),
+    path('goals/add/', views.add_goal, name='add_goal'),
+    path('goals/update/<int:goal_id>/', views.update_goal, name='update_goal'),
+    path('goals/update_amount/<int:goal_id>/', views.update_goal_amount, name='update_goal_amount'),
+    path('goal/delete/<int:goal_id>/', views.delete_goal, name='delete_goal'),
 ]
 
